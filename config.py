86ec95813fb1db1c9e8b6f2a5d4c91cf1b94c2ef
GreatnessGrace <graceunstoppable7@gmail.com>
2025-03-28 15:41:26 +0530

    config

--- config.py
+++ config.py
@@ -1,13 +1,27 @@
 # config.py
 
-BASE_URL = "https://www.theknot.com/marketplace/wedding-reception-venues-atlanta-ga"
-CSS_SELECTOR = "[class^='info-container']"
-REQUIRED_KEYS = [
-    "name",
-    "price",
-    "location",
-    "capacity",
-    "rating",
-    "reviews",
-    "description",
-]
+# BASE_URL = "https://www.theknot.com/marketplace/wedding-reception-venues-atlanta-ga"
+# CSS_SELECTOR = "[class^='info-container']"
+# REQUIRED_KEYS = [
+#     "name",
+#     "price",
+#     "location",
+#     "capacity",
+#     "rating",
+#     "reviews",
+#     "description",
+# ]
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env
+load_dotenv()
+
+# Get token from environment variable
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+
+BASE_URL = "https://api.github.com/search/code?q=APT41+IOC"
+HEADERS = {
+    "Accept": "application/vnd.github.v3+json",
+    "Authorization": f"token {GITHUB_TOKEN}",
+}
